8dba2c70e582bf58e7f1e6a3cf7fb97702dcabfc
ohiduzzaman <ohiduzzaman23@gmail.com>
2025-12-16 22:58:49 +0600

    Comment reply update

--- index.js
+++ index.js
@@ -200,7 +200,7 @@ async function run() {
     app.post("/lessons/:id/report", verifyJWT, async (req, res) => {
       const { reason } = req.body;
       const userEmail = req.tokenEmail;
-      // new report
+
       const newReport = {
         _id: new ObjectId(),
         user: userEmail,
@@ -240,7 +240,7 @@ async function run() {
       res.send(newComment);
     });
 
-    // Add reply
+    // Add comment reply
     app.post(
       "/lessons/:id/comments/:commentId/replies",
       verifyJWT,
